16d6024283ffa82e319b0074de7800c19319c104
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-27 22:44:04 +0700

    initail commit (finish post api)

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,24 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired // inject PersonService to PersonController
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping // post method
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+}
